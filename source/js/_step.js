0ccd3eab02b0be87865290e9bed34f73bbf53600
nguyenhuyen195 <nguyen.my.huyen@sun-asterisk.com>
2021-01-12 13:53:11 +0700

    fix comment

--- source/js/_step.js
+++ source/js/_step.js
@@ -1,23 +1,14 @@
 $(document).ready(function() {
-  $('.step-content').first().show();
+  $('.auth-block--step').first().show();
 });
 
-$('.next').click(function(event) {
-  var currentStep = $(this).closest('.step-content');
-  var nextStep = $(this).closest('.step-content').next();
-  $('.step').eq($('.step-content').index(currentStep)).removeClass('step__current');
-  $('.step').eq($('.step-content').index(nextStep)).addClass('step__current');
-  currentStep.hide();
-  nextStep.show();
-  event.preventDefault();
-});
+$(document).on('click', '.js-step', function(e) {
+  var nextStep = $(this).attr('data-show');
+  var currentStep = $(this).parents('.auth-block--step');
 
-$('.back').click(function(event) {
-  var currentStep = $(this).closest('.step-content');
-  var prevStep = $(this).closest('.step-content').prev();
-  $('.step').eq($('.step-content').index(currentStep)).removeClass('step__current');
-  $('.step').eq($('.step-content').index(prevStep)).addClass('step__current');
+  $(`#${currentStep.attr('id')}-label`).removeClass('step--current');
   currentStep.hide();
-  prevStep.show();
-  event.preventDefault();
+  $(`${nextStep}-label`).addClass('step--current');
+  $(nextStep).show();
+  e.preventDefault();
 });
